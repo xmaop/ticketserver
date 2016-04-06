738121c4297fcc31e49dba6a38663b7aeb620196
xmaop <morellana@gmail.com>
2016-04-06 05:48:23 +0000

    Se agreg'o clase ticket, se genera ticket

---
+++ class/ticket.php
@@ -0,0 +1,15 @@
+<?php
+class Ticket {
+
+    public $id;
+    public $idUser;
+    public $idCompany;
+    public $companyName;
+    public $companyBranch;
+    public $numberTicket;
+    public $peopleQuantity;
+    public $timeLeft;
+    public $serverTime;
+    public $status;
+
+} ?>
--- token.php
+++ token.php
@@ -1,7 +1,11 @@
 <?php
+require_once('class/ticket.php');
 $token=$_GET['token']."\n";
 file_put_contents('lista.txt', $token, FILE_APPEND | LOCK_EX);
 $current= rand(1, 15000);
 file_put_contents('dato.txt', $current);
-echo $current;
+$ticket = new ticket;
+$ticket->id=123;
+$ticket->user="miguel";
+echo json_encode($ticket);
 ?>
